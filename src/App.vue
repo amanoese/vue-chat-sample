baebe05d0d135f234295685ade0b0734bfef8e6f
Seito Taka <amanoese@gmail.com>
2019-01-14 22:47:42 +0900

    create sample

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr>
+    <button v-if="user.uid" @click="doLogout">logout!</button>
+    <button v-else="user.uid" @click="doLogin">login!</button>
+    <hr>
+    <input type="text" v-model="sendText" @keyup.enter.prevent="doSend"/>
+    <hr>
+    <p v-for="chat in chats">{{chat.name}}:{{chat.text}}</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import firebase from 'firebase'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      user : {},
+      chats : [],
+      sendText : ''
+    }
+  },
   components: {
     HelloWorld
+  },
+  created(){
+    firebase.auth().onAuthStateChanged(user => {
+      if(!user) { return }
+      // ログイン状態ならuserが取得できる
+      this.user = user
+      const ref_message = firebase.database().ref('message')
+      console.log({ref_message})
+      console.log(ref_message.key)
+      ref_message.limitToLast(10).on('child_added', this.childAdded.bind(this))
+      ref_message.limitToLast(10).off('child_added', this.childAdded.bind(this))
+    })
+  },
+  methods : {
+    doLogin(){
+      const provider = new firebase.auth.GithubAuthProvider();
+      firebase.auth().signInWithPopup(provider)
+    },
+    doLogout() {
+      firebase.auth().signOut().then(()=>{
+        this.user = {}
+      })
+    },
+    doSend() {
+      console.log('enter!',this.user,this.sendText);
+      if (!this.user.uid || !this.sendText.length) { return }
+
+      console.log('send!')
+
+      firebase.database().ref('message').push({
+        text: this.sendText,
+        name: this.user.displayName
+      }, () => {
+        this.sendText = '' // フォームを空にする
+      })
+    },
+    childAdded(snap) {
+      this.chats.push(snap.val())
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import firebase from 'firebase'
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = { };
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App),
